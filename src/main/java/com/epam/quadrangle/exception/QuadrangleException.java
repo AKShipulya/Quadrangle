abf7c23dde0a9fc38ea9f23d63b443156664443e
Andrey Shipulya <andrey.shipulya@bmw.by>
2021-11-30 12:43:18 +0300

    created QuadrangleException

---
+++ src/main/java/com/epam/quadrangle/exception/QuadrangleException.java
@@ -0,0 +1,19 @@
+package com.epam.quadrangle.exception;
+
+public class QuadrangleException extends Exception {
+
+    public QuadrangleException() {
+    }
+
+    public QuadrangleException(String message) {
+        super(message);
+    }
+
+    public QuadrangleException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public QuadrangleException(Throwable cause) {
+        super(cause);
+    }
+}
